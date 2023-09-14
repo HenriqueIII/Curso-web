4a2c9752451f8d726a976fac66d5777f875bb594
Paulo Gonçalves <paulogoncalves.m@gmail.com>
2023-09-14 21:03:54 +0100

    Começando o modulo de OO do curso de programação WEB 	new file:   objecto/criandoObjectos.js 	new file:   objecto/funcoesImportantes.js 	new file:   objecto/getterSetter.js 	new file:   objecto/notacaoLiteral.js 	new file:   objecto/objectoRevisao.js 	new file:   objecto/objectosConstantes.js 	new file:   objecto/oo.js

---
+++ objecto/oo.js
@@ -0,0 +1,22 @@
+// CÓDIGO NÃO EXECUTÁVEL
+
+// Procedural
+processamento (valor1, valor2, valor3)
+
+// OO
+objecto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento() {
+        // ...
+    }
+}
+
+objecto.processamento() // Foco passou a ser o objecto
+
+// Principios Importantes
+    // 1. Abstração
+    // 2. Encapsulamento
+    // 3. Herança(prototype)
+    // 4. Polimorfismo 
